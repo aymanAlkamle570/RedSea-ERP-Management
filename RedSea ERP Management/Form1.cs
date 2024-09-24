99a8a7988a3d549d5bcc758596a13d29d559bd01
notion-tech <notion-tech@AYMAN>
2024-09-24 19:56:08 +0300

    f

--- RedSea ERP Management/Form1.cs
+++ RedSea ERP Management/Form1.cs
@@ -1,11 +1,15 @@
+using System.Data;
+
 namespace RedSea_ERP_Management
 {
     public partial class Form1 : Form
     {
+        InteractiveServer IS = new InteractiveServer();
         public Form1()
         {
             InitializeComponent();
-
+            timer1.Start();
+            label1.Text = DBController.GetInfos(Properties.Settings.Default.Db_name, new string[] { "udn" }, "users", $"uname='{Properties.Settings.Default.def_uname}'").Rows[0][0].ToString();
         }
 
         private void guna2ImageButton1_Click(object sender, EventArgs e)
@@ -30,7 +34,70 @@ namespace RedSea_ERP_Management
 
         private void button1_Click(object sender, EventArgs e)
         {
-           guna2TabControl1.TabMenuVisible = ! guna2TabControl1.TabMenuVisible;
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            WindowState = FormWindowState.Minimized;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            tabControl1.SelectedIndex = 0;
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            tabControl1.SelectedIndex = 1;
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            tabControl1.SelectedIndex = 2;
+
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            tabControl1.SelectedIndex = 3;
+
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            tabControl1.SelectedIndex = 4;
+
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            tabControl1.SelectedIndex = 5;
+
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            tabControl1.SelectedIndex = 6;
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            label2.Text = IS.GetData(DateTime.Now.ToString("ddd yyyy/MM/dd \n hh:mm:ss tt")).ToString();
+
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            Properties.Settings.Default.FirstTime = true;
+            Properties.Settings.Default.Save();
         }
     }
 }
---
+++ RedSea ERP Management/Wizard.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RedSea_ERP_Management
+{
+    public partial class Wizard : Form
+    {
+        public Wizard()
+        {
+            InitializeComponent();
+            if (dbname.Text.Length != 0 && servername.Text.Length != 0 && password.Text.Length != 0 && adminname.Text.Length != 0 && adminpassword.Text.Length != 0)
+            {
+                button1.Enabled
+                    = true;
+            }
+        }
+
+        private void flowLayoutPanel2_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (password.Text == Properties.Settings.Default.password)
+                {
+                    DBController.CreateDB(dbname.Text);
+                    Properties.Settings.Default.Db_name = dbname.Text;
+
+                    DBController.CreateTable(dbname.Text,"users", new Dictionary<string, string>()
+            {
+                { "id","int primary key identity  not null" }
+                ,{"uname","nvarchar(200)"},{"upass","nvarchar(200)"},{"udn","nvarchar(200)"}
+            });
+
+                    DBController.CreateTable(dbname.Text, "perm", new Dictionary<string, string>()
+            {
+                {"u_id","int" },{"action_id","int"},{"action_allowed","bit"}
+            });
+                    DBController.CreateTable(dbname.Text, "orders", new Dictionary<string, string>()
+            {
+                {"o_id","int" },{"customer_id","int"},{"state","int"},{"o_sum","float"},{"order_dt","nvarchar(100)"},{"u_id","int"}
+            });
+                    DBController.CreateTable(dbname.Text, "order_pros", new Dictionary<string, string>()
+      {
+          {"o_id","int" },{"pro_id","int"},{"state","bit"},{"customer_id","int"}
+      });
+                    DBController.CreateTable(dbname.Text, "offers", new Dictionary<string, string>()
+            {
+                {"off_id","int" },{"supplier_id","int"},{"state","int"},{"o_sum","float"},{"order_dt","nvarchar(100)"},{"u_id","int"}
+            });
+                    DBController.CreateTable(dbname.Text, "offer_pros", new Dictionary<string, string>()
+      {
+          {"off_id","int" },{"pro_id","int"},{"state","bit"},{"supplier_id","int"}
+      });
+
+                    DBController.InsertInto(dbname.Text, "users", new Dictionary<string, string>()
+            {
+                {"uname",adminname.Text },{"upass",adminpassword.Text },{ "udn","admin"}
+            });
+                    Properties.Settings.Default.FirstTime = false;
+                    Properties.Settings.Default.Save();
+                    Process.Start(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, $"{nameof(RedSea_ERP_Management).Replace("_"," ")}.exe"));
+                    Application.Exit();
+                }
+                else
+                {
+                    MessageBox.Show(new UnauthorizedAccessException().Message, "Wrong Password", MessageBoxButtons.OK, MessageBoxIcon.Error, MessageBoxDefaultButton.Button1);
+                }
+
+            }
+            catch (SqlException ex)
+            {
+                MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error, MessageBoxDefaultButton.Button1);
+
+            }
+          
+        }
+
+        private void Validate(object sender, EventArgs e)
+        {
+            if (dbname.Text.Length != 0 && servername.Text.Length != 0 && password.Text.Length != 0 && adminname.Text.Length != 0 && adminpassword.Text.Length != 0)
+            {
+                button1.Enabled
+                    = true;
+            }
+        }
+    }
+}
